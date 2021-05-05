ecbbc97f49dbd84ce01f0a229638eebf300577b8
vince167861 <vince167861@gmail.com>
2021-05-05 16:39:02 +0800

    Rename Portal sprite name
    Signed-off-by: vince167861 <vince167861@gmail.com>

--- Assets/Scripts/InvisBrickHandler.cs
+++ Assets/Scripts/InvisBrickHandler.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class InvisBrickHandler : MonoBehaviour
 {
-    public Sprite blockTexture;
+    public Sprite sprite;
     void OnCollisionEnter2D(Collision2D col)
     {
         switch (col.collider.tag)
         {
             case "Slime":
-                GetComponent<SpriteRenderer>().sprite = blockTexture;
+                GetComponent<SpriteRenderer>().sprite = sprite;
                 break;
         }
     }
