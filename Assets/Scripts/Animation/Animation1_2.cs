475fc298429e5ef7d354df26ca9590d05092408b
vince167861 <vince167861@gmail.com>
2021-05-05 15:01:54 +0800

    Fixed 'Object Null Reference' fatal
    Signed-off-by: vince167861 <vince167861@gmail.com>

---
+++ Assets/Scripts/Animation/Animation1_2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation1_2 : Animation
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Animation.handler = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void handle()
+    {}
+
+    public override void trigger(int triggerId)
+    {}
+}
