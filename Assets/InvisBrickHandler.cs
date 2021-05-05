007505693d8dc1dee6d02ff47c61dafbc6c34373
vince167861 <vince167861@gmail.com>
2021-05-05 16:22:12 +0800

    Add behavior of invis block
    Signed-off-by: vince167861 <vince167861@gmail.com>

---
+++ Assets/InvisBrickHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvisBrickHandler : MonoBehaviour
+{
+    public Sprite blockTexture;
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        switch (col.collider.tag)
+        {
+            case "Slime":
+                GetComponent<SpriteRenderer>().sprite = blockTexture;
+                break;
+        }
+    }
+}
