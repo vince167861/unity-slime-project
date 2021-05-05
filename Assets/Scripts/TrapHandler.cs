58862a30e3a0452bc9d5d46b913c05e5bd9a81a8
vince167861 <vince167861@gmail.com>
2021-05-05 15:52:25 +0800

    Add trap prefab
    Signed-off-by: vince167861 <vince167861@gmail.com>

---
+++ Assets/Scripts/TrapHandler.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapHandler : MonoBehaviour , Attackable
+{
+    public int AttackDamage { get => 6; }
+
+
+}
