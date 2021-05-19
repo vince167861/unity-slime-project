c45912f7b8b9515b5d6c2f557f0b3fb706ec5da2
BrianGodd <brianbaby0409@gmail.com>
2021-05-19 17:27:47 +0800

    5/19

--- Assets/Scripts/stopbottomh.cs
+++ Assets/Scripts/stopbottomh.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class stopbottomh : MonoBehaviour
 {
-    GameGlobalController.GameState nowState;
+    public Sprite[] stop = new Sprite[2];
+    GameGlobalController.GameState nowState,prenowState;
     // Start is called before the first frame update
     public void GameStop()
     {
         nowState = GameGlobalController.gameState;
+        prenowState = nowState;
         GameGlobalController.gameState = GameGlobalController.GameState.Pause;
     }
     public void GameStart()
     {
+        nowState = prenowState;
         GameGlobalController.gameState = nowState;
+    }
+    void Start()
+    {
+    }
+    void Update(){
+        if(GameGlobalController.gameState == GameGlobalController.GameState.Instruction){
+            gameObject.GetComponent<Image>().sprite = stop[1];
+        }
+        else gameObject.GetComponent<Image>().sprite = stop[0];
     }
 }
