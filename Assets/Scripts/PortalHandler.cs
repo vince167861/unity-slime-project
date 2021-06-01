f27a5f1b366ee85d273c5f260d5afa80cdfafbde
BrianGodd <brianbaby0409@gmail.com>
2021-06-01 10:03:17 +0800

    0601

--- Assets/Scripts/PortalHandler.cs
+++ Assets/Scripts/PortalHandler.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class PortalHandler : MonoBehaviour
 {
     Animator animator;
+    static TextMeshPro need;
     bool trigger = false;
     float delta = 3;
     // Start is called before the first frame update
     void Start()
     {
+        need = GameObject.Find("needkey").GetComponent<TextMeshPro>();
         animator = GetComponent<Animator>();
     }
 
@@ -23,8 +27,9 @@ public class PortalHandler : MonoBehaviour
                 break;
             case GameGlobalController.GameState.Playing:
             case GameGlobalController.GameState.Lobby:
+                need.text = " x " + LevelVarity.doorKey[GameGlobalController.currentLevel];
                 delta += Time.deltaTime;
-                if (Input.GetKey(KeyCode.G) && trigger)
+                if (Input.GetKey(KeyCode.G) && trigger && (GameGlobalController.slimeInstance.GetComponent<Slime>().keyCount >= LevelVarity.doorKey[GameGlobalController.currentLevel]))
                 {
                     MainCameraHandler.allSound = 7;
                     animator.Play("opendoor");
