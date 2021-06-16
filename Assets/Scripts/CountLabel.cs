49762a3683fce0a2cbba9f0a300aa6e424b844b2
BrianGodd <brianbaby0409@gmail.com>
2021-06-16 21:27:30 +0800

    0616

---
+++ Assets/Scripts/CountLabel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountLabel : MonoBehaviour
+{
+	Animator animator;
+	Text countText;
+
+	private void Start()
+	{
+		countText = GetComponentInChildren<Text>();
+		animator = GetComponent<Animator>();
+	}
+
+	public void updateCount(int count)
+	{
+		animator.Play("update");
+		countText.text = " X  " + count;
+	}
+}
