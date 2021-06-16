e951f9b7f87c7e611aad1afaee41d5aef7cace5b
BrianGodd <brianbaby0409@gmail.com>
2021-06-16 16:51:52 +0800

    0616

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,40 +2,41 @@
 
 public class Bullet : MonoBehaviour, Attackable
 {
-    public float moveSpeed = 0.3f; //bullet movement speed
+	public float moveSpeed = 0.3f; //bullet movement speed
 
-    public int AttackDamage => 50;
+	public int AttackDamage => 50;
 
-    void Start()
-    {
-        if (moveSpeed < 0)
-            transform.localScale = new Vector3(-1, 1, 1);
-        if (moveSpeed > 0)
-            transform.localScale = new Vector3(1, 1, 1);
-    }
+	void Start()
+	{
+		if (moveSpeed < 0)
+			transform.localScale = new Vector3(-1, 1, 1);
+		if (moveSpeed > 0)
+			transform.localScale = new Vector3(1, 1, 1);
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        switch (GameGlobalController.gameState)
-        {
-            case GameGlobalController.GameState.Lobby:
-                Destroy(gameObject);
-                break;
-            case GameGlobalController.GameState.Playing:
-                transform.Translate(moveSpeed, 0, 0);
-                break;
-        }
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		switch (GameGlobalController.gameState)
+		{
+			case GameGlobalController.GameState.Lobby:
+				Destroy(gameObject);
+				break;
+			case GameGlobalController.GameState.Playing:
+				transform.Translate(moveSpeed, 0, 0);
+				break;
+		}
+	}
 
-    void OnTriggerEnter2D(Collider2D collision)
-    {
-        switch (collision.tag)
-        {
-            case "bird":
-                collision.GetComponent<Entity>().Suffer(AttackDamage);
-                Destroy(gameObject);
-                break;
-        }
-    }
+	void OnTriggerEnter2D(Collider2D collision)
+	{
+		switch (collision.tag)
+		{
+			case "bird":
+			case "Mushroom":
+				collision.GetComponent<Entity>().Suffer(AttackDamage);
+				Destroy(gameObject);
+				break;
+		}
+	}
 }
--- Assets/Scripts/Mushroom.cs
+++ Assets/Scripts/Mushroom.cs
@@ -1,45 +1,44 @@
-﻿using System.Collections;
+﻿#pragma warning disable CS0108 // 無法辨認的 #pragma 指示詞
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Mushroom : Entity, Attackable
 {
-    public Mushroom() : base(1, 0) {}
-    public int AttackDamage => 40;
-    public float jumpSpan = 0, jumpWait = 0;
+	public Mushroom() : base(1, 0) { }
+	public int AttackDamage => 40;
+	public float jumpSpan = 0, jumpWait = 0;
+	private Animator animator;
+	public float multiplier = 0.5f;
 
-    void OnTriggerStay2D(Collider2D collider)
-    {
-        switch(collider.tag)
-        {
-            case "Slime":
-                direction = (Slime.transform.position.x - transform.position.x) > 0 ? 1 : -1;
-                break;
-        }
-    }
+	private void Start()
+	{
+		animator = GetComponent<Animator>();
+	}
 
-    void OnCollisionStay2D(Collision2D collision)
-    {
-        switch(collision.collider.tag)
-        {
-            case "Ground":
-                jumpSpan += Time.deltaTime;
-                if (jumpSpan >= jumpWait)
-                {
-                    GetComponent<Rigidbody2D>().AddForce(new Vector3(direction * 100, 300, 0));
-                }
-                break;
-        }
-    }
+	void OnCollisionStay2D(Collision2D collision)
+	{
+		switch (collision.collider.tag)
+		{
+			case "Ground":
+				jumpSpan += Time.deltaTime;
+				if (jumpSpan >= jumpWait)
+				{
+					animator.Play("Jump");
+					GetComponent<Rigidbody2D>().AddForce(new Vector3(direction * 100, 300, 0) * multiplier);
+				}
+				break;
+		}
+	}
 
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        switch (collision.collider.tag)
-        {
-            case "Ground":
-                jumpSpan = 0;
-                jumpWait = Random.Range(0.5f, 1.5f);
-                break;
-        }
-    }
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		switch (collision.collider.tag)
+		{
+			case "Ground":
+				jumpSpan = 0;
+				jumpWait = Random.Range(0.5f, 1.5f);
+				break;
+		}
+	}
 }
