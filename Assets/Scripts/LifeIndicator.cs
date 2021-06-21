5cb84210bebe5bf6f8713a1b3f161c8281edccba
BrianGodd <brianbaby0409@gmail.com>
2021-06-21 14:01:58 +0800

    0621

--- Assets/Scripts/LifeIndicator.cs
+++ Assets/Scripts/LifeIndicator.cs
@@ -13,7 +13,7 @@ public class LifeIndicator : MonoBehaviour
 	{
 		parent = GetComponentInParent<Entity>();
 		fillings = transform.Find("Sprite Mask/Fillings");
-		fillColor = transform.Find("Sprite Mask/Filling").GetComponent<SpriteRenderer>();
+		fillColor = fillings.GetComponent<SpriteRenderer>();
 		Name = transform.Find("Name");
 	}
 
@@ -24,10 +24,10 @@ public class LifeIndicator : MonoBehaviour
 			case GameGlobalController.GameState.Animation:
 			case GameGlobalController.GameState.Shaking:
 			case GameGlobalController.GameState.Playing:
-				if(fillings.localScale.x <= 0) parent.Die();
+				if(fillings.localScale.x <= 0.02) parent.Die();
 				transform.localScale = new Vector3(-1 * parent.direction, 1, 1);
 				Name.GetComponent<TextMeshPro>().text = parent.name;
-				fillColor.color = Color.HSVToRGB(0.75f + (parent.healthPercentage - 1)*0.75f, 1, 1);
+				fillColor.color = Color.HSVToRGB(0.25f + (parent.healthPercentage - 1) * 0.25f, 1, 1);
 				if (fillings.localScale.x - parent.healthPercentage > 0.02)
 					fillings.localScale -= new Vector3(0.01f, 0, 0);
 				if (fillings.localScale.x - parent.healthPercentage < -0.02)
