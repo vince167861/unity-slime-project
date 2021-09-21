bc62da9ec682b13e4c76b849b0cbf2217a2b3b03
vince167861 <vince167861@gmail.com>
2021-09-21 10:17:43 +0800

    Format Trap.cs

--- Assets/Scripts/Trap.cs
+++ Assets/Scripts/Trap.cs
@@ -1,18 +1,18 @@
 ﻿using UnityEngine;
 
-public class Trap : MonoBehaviour , IAttackable
+public class Trap : MonoBehaviour, IAttackable
 {
-    public int AttackDamage => 150;
-    void OnTriggerEnter2D(Collider2D collider)
-    {
-        switch(collider.tag)
-        {
-            case "Slime":
-                MainCameraHandler.PlayEntityClip(10);
-                Game.lastattack = 2;
-                collider.GetComponentInParent<Entity>().Suffer(AttackDamage);
-                break;
-        }
-    }
+	public int AttackDamage => 150;
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		switch (collider.tag)
+		{
+			case "Slime":
+				MainCameraHandler.PlayEntityClip(10);
+				Game.lastattack = 2;
+				collider.GetComponentInParent<Entity>().Suffer(AttackDamage);
+				break;
+		}
+	}
 }
 
